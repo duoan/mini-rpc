c0906d4711119fa0b2cc6ed19a87459eefd1ceae
anduo <duoan@amazon.com>
2018-01-27 23:51:29 +0800

    🌈🌈🌈add travis ci.🌈🌈🌈

--- rpc-example/src/test/java/me/anduo/rpc/example/HelloServiceTest.java
+++ rpc-example/src/test/java/me/anduo/rpc/example/HelloServiceTest.java
@@ -1,7 +1,8 @@
 package me.anduo.rpc.example;
 
 
-
+import me.anduo.rpc.client.RpcProxy;
+import me.anduo.rpc.server.modules.simple.HelloService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,25 +10,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import xyz.anduo.rpc.client.RpcProxy;
-import xyz.anduo.rpc.sieve.modules.simple.HelloService;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:spring-client.xml")
 public class HelloServiceTest {
 
-	@Autowired
+    @Autowired
     private RpcProxy rpcProxy;
- 
+
     @Test
     public void helloTest() {
         try {
-			HelloService helloService = rpcProxy.create(HelloService.class);
-			String result = helloService.hello("World");
-			Assert.assertEquals("Hello! World", result);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            HelloService helloService = rpcProxy.create(HelloService.class);
+            String result = helloService.hello("World");
+            Assert.assertEquals("Hello! World", result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
